51ffe8c9ec14be3a7173c64915d2632d3fa96978
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-15 18:30:29 +0530

    alert

--- app/js/app.js
+++ app/js/app.js
@@ -15,4 +15,18 @@ settingsForm.addEventListener('submit', event => {
     const alertFrequency = document.getElementById('alert-frequency').value;
 
     ipcRenderer.send('settings:set', { cpuOverload, alertFrequency }); // sending data to main process
+    showAlert("Settings Saved");
 });
+
+// show alert for settings
+function showAlert(msg) {
+    const alert = document.getElementById('alert');
+    alert.classList.remove('hide');
+    alert.classList.add('alert');
+    alert.innerText = msg;
+
+    setTimeout(() => {
+        alert.classList.remove('alert');
+        alert.classList.add('hide');
+    }, 3000)
+}
