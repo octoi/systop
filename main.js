ce12c195ee06e08ad741a38dc86a713bfaa13a8c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-15 18:48:04 +0530

    tray icon

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require('electron')
+const path = require('path');
 const Store = require('./Store');
 
-process.env.NODE_ENV = 'development'
+// process.env.NODE_ENV = 'development'
 
 const isDev = process.env.NODE_ENV === 'development';
 const isMac = process.platform === 'darwin';
 
 let mainWindow;
+let tray;
 
 // init store & default
 const store = new Store({
@@ -25,7 +27,9 @@ function createMainWindow() {
         width: isDev ? 800 : 355,
         height: 600,
         icon: `${__dirname}/assets/icons/icon.png`,
-        resizable: isDev ? true : false,
+        resizable: isDev,
+        show: false,
+        opacity: 0.9,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -39,11 +43,24 @@ function createMainWindow() {
     mainWindow.loadFile('./app/index.html')
 }
 
+
 app.on('ready', () => {
     createMainWindow()
 
     mainWindow.webContents.on('dom-ready', () => {
         mainWindow.webContents.send('settings:get', store.get('settings'));
+    });
+
+    const icon = path.join(__dirname, 'assets', 'icons', 'tray_icon.png');
+    tray = new Tray(icon);
+
+    tray.on('click', () => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            console.log("show")
+            mainWindow.show();
+        }
     });
 
     const mainMenu = Menu.buildFromTemplate(menu)
