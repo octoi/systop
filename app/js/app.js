f3889b10cc800ba544fd229c64631618cd9dfb14
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-15 18:26:51 +0530

    sending settings

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,18 @@
 const { ipcRenderer } = require('electron');
+const settingsForm = document.getElementById('settings-form');
 
-ipcRenderer.on('settings:get', (e, settings) => {
+// get settings
+ipcRenderer.on('settings:get', (_, settings) => {
     document.getElementById('cpu-overload').value = settings.cpuOverload;
     document.getElementById('alert-frequency').value = settings.alertFrequency;
+});
+
+// submit settings
+settingsForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const cpuOverload = document.getElementById('cpu-overload').value;
+    const alertFrequency = document.getElementById('alert-frequency').value;
+
+    ipcRenderer.send('settings:set', { cpuOverload, alertFrequency }); // sending data to main process
 });
